7a0f478c0f2cef076124ef1186454cc082dfb5f5
daantoma <daantoma@hotmail.com>
2020-11-19 22:13:52 -0500

    ajustando nombre de la clase instanciada del service

--- src/app/cliente/cliente-registro/cliente-registro.page.ts
+++ src/app/cliente/cliente-registro/cliente-registro.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { Cliente } from 'src/app/models/cliente';
 import { ClienteServiceService } from 'src/app/services/cliente-service.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-cliente-registro',
@@ -25,11 +26,13 @@ export class ClienteRegistroPage implements OnInit {
   datosRetornadosDelServicio:any="";
 
   constructor(private clienteService: ClienteServiceService,
+    private alertController: AlertController,
     private navContrl:NavController
     ) { }
 
   ngOnInit() {
-    this.cliente = new Cliente();
+    
+    this.cliente = new Cliente(); 
     this.traerClientes();
   }
 
@@ -39,6 +42,12 @@ export class ClienteRegistroPage implements OnInit {
         (datos)=>{this.clientes=datos;},
         (error)=>{console.log(error);}
       );
+  }
+
+  traerClienteSeleccionado(valor:string){
+    this.clienteService.getId(valor).subscribe(p => {
+        this.presentAlert(p);
+    });
   }
 
   template() {
@@ -51,11 +60,70 @@ export class ClienteRegistroPage implements OnInit {
     this.cliente.sexo = this._sexo;
     this.cliente.usuario = this._usuario;
     console.log(this.cliente);
-    this.clienteService.add(this.cliente).subscribe((datosRetornadosDelServicio)=>{
+    this.clienteService.post(this.cliente).subscribe((datosRetornadosDelServicio)=>{
       this.datosRetornadosDelServicio = JSON.stringify(datosRetornadosDelServicio);
     });
+  }
 
+  async VerCliente(idc:string) {
+    
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Alert',
+      inputs: [
+        {
+          name: 'name1',
+          type: 'text',
+          placeholder: 'Placeholder 1'
+        }],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        },
+        {
+          text: 'Ok',
+          handler: () => {
+            console.log('Confirm Ok');
+          }
+        },
+        {
+          text: 'Ok',
+          handler: () => {
+            console.log('Confirm Ok');
+          }
+        }
+      ]
+    });
 
+    await alert.present();
+  }
+
+  async presentAlert(clientec:Cliente) {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: clientec.nombre,
+      subHeader: clientec.identificacion,
+      message: 'Cliente encontrado. pRUEBA PARA ELIMINAR',
+      buttons: [{
+        text: 'ELIMINAR?',
+        handler: () => {
+          this.delete(clientec.identificacion);
+        }
+      }]
+    });
+
+    await alert.present();
+  }
+
+  delete(identificacion: string) {
+    this.clienteService.delete(identificacion).subscribe(p => {
+      console.log('CLIENTE: '+identificacion+' eliminado');
+    });
   }
 
 }
